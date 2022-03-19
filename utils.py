a2b7858bc0e22b47354595236430b6b5b39e2ccd
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-19 21:39:24 +0600

    chore: Добавил файл utils.py и обозначил в нем функции

---
+++ utils.py
@@ -0,0 +1,16 @@
+
+
+def load_candidates_from_json(json_file='candidates.json'):
+    pass
+
+
+def get_candidate_by_id():
+    pass
+
+
+def get_candidate_by_name():
+    pass
+
+
+def get_candidate_by_skill():
+    pass
