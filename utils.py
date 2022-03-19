19e839e08a396ac3bf7ac1cc4b36fea8ee574561
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-19 21:45:10 +0600

    chore: Функция чтения данных. Минимальное Flask приложение.

--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
+import json
+from flask import Flask
 
-
-def load_candidates_from_json(json_file='candidates.json'):
-    pass
+def load_candidates(filename='candidates.json'):
+    """
+    Функция читает json с вопросами и возвращает словарь для игры
+    :param filename: По умолчанию questions.json
+    :return: Словарь
+    """
+    file = open(filename, encoding='UTF-8')
+    candidates_list = json.load(file)
+    file.close()
+    return candidates_list
 
 
 def get_candidate_by_id():
@@ -14,3 +23,17 @@ def get_candidate_by_name():
 
 def get_candidate_by_skill():
     pass
+
+
+# ЗАГРУЗКА ВОПРОСОВ в список
+candidates_list = load_candidates()
+
+# ЗАПУСК ПРИЛОЖЕНИЯ ФЛАСК
+app = Flask(__name__)
+
+@app.route("/")
+def page_index():
+    all_candidates = candidates_list
+    return f"<pre>{all_candidates}<pre>"
+
+app.run()
